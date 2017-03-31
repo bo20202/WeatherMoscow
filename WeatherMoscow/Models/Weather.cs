8708f0e4daf950f51bb847d8a6ad2cafd990d5af
bo20202 <bo20202@yandex.ru>
2017-03-31 17:27:21 +0300

    Update libs and init model

---
+++ WeatherMoscow/Models/Weather.cs
@@ -0,0 +1,39 @@
+namespace WeatherMoscow.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class WeatherContext : DbContext
+    {
+        public WeatherContext()
+            : base("name=Weather")
+        {
+        }
+
+
+         public DbSet<Weather> WeatherObjects { get; set; }
+    }
+
+    public class Weather
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int AirTemp { get; set; }
+        public int Moisture { get; set; }
+        public int DewPoint { get; set; }
+
+        public int Pressure { get; set; }
+
+        public string WindDir { get; set; }
+        public int? WindSpeed { get; set; }
+
+        public int Cloudness { get; set; }
+        public int CloudBase { get; set; }
+        public int HorizontalVisibility { get; set; }
+
+
+    }
+}
