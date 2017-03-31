902a680d77d4de794ba6aba44c1ad0b0ce884a36
bo20202 <bo20202@yandex.ru>
2017-04-01 01:59:50 +0300

    Model update

--- WeatherMoscow/Models/Weather.cs
+++ WeatherMoscow/Models/Weather.cs
@@ -2,7 +2,7 @@ namespace WeatherMoscow.Models
 {
     using System;
     using System.Data.Entity;
-    using System.Linq;
+    using System.ComponentModel.DataAnnotations;
 
     public class WeatherContext : DbContext
     {
@@ -12,28 +12,41 @@ namespace WeatherMoscow.Models
         }
 
 
-         public DbSet<Weather> WeatherObjects { get; set; }
+         public DbSet<Weather> Weathers { get; set; }
     }
 
     public class Weather
     {
         public int Id { get; set; }
-
+        
+        [Display(Name = "Дата")]
         public DateTime Date { get; set; }
+        [Display(Name = "Время")]
+        public TimeSpan Time { get; set; }
 
-        public int AirTemp { get; set; }
-        public int Moisture { get; set; }
-        public int DewPoint { get; set; }
+        [Display(Name = "Температура")]
+        public double? AirTemp { get; set; }
+        [Display(Name = "Влажность")]
+        public double? Moisture { get; set; }
+        [Display(Name = "Точка росы")]
+        public double? DewPoint{ get; set; }
+        
+        [Display(Name = "Давление")]
+        public int? Pressure { get; set; }
 
-        public int Pressure { get; set; }
-
+        [Display(Name = "Направление ветра")]
         public string WindDir { get; set; }
+        [Display(Name = "Скорость ветра")]
         public int? WindSpeed { get; set; }
 
-        public int Cloudness { get; set; }
-        public int CloudBase { get; set; }
-        public int HorizontalVisibility { get; set; }
+        [Display(Name = "Облачность")]
+        public int? Cloudness { get; set; }
+        [Display(Name = "Нижняя граница облачности")]
+        public int? CloudBase { get; set; }
+        [Display(Name = "Горизонтальная видимость")]
+        public int? HorizontalVisibility { get; set; }
 
+        [Display(Name = "Погодные условия")]
         public string WeatherConditions { get; set; }
 
     }
